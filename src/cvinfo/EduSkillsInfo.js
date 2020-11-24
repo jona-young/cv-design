a4c69cd0f8e599afa7ec346c13368cd48ed525f1
jonayoung <young.jon@icloud.com>
2020-11-23 21:36:53 -0500

    added some info

--- src/cvinfo/EduSkillsInfo.js
+++ src/cvinfo/EduSkillsInfo.js
@@ -7,16 +7,34 @@ function EduSkillsInfo () {
             <div className="circle">
             </div>
             <div>
-                <p><b>Address: </b>123 Numerical Blvd</p>
-                <p><b>Phone: </b>(123) 456-7890</p>
-                <p><b>Email: </b>email@email.com</p>
-                <p><b>Github: </b>jona-young</p>
+                <p className="eduskills__contact"><b>Address: </b>123 Numerical Blvd</p>
+                <p className="eduskills__contact"><b>Phone: </b>(123) 456-7890</p>
+                <p className="eduskills__contact"><b>Email: </b>email@email.com</p>
+                <p className="eduskills__contact"><b>Github: </b>jona-young</p>
             </div>
             <div className="eduskills__education">
-                education info
+                <p className="eduskills__title">
+                    EDUCATION
+                </p>
+                <p className="eduskills__major">
+                    Communications, Culture, and Information Technology
+                </p>
+                <p className="eduskills__school">
+                    University of Toronto
+                    <br />
+                    2010 - 2015
+                </p>
+                <p className="eduskills__major">
+                    Sport Management
+                </p>
+                <p className="eduskills__school">
+                    Humber College
+                    <br />
+                    2015-2018
+                </p>
             </div>
-            <div className="eduskills_keys">
-                key skills info
+            <div className="eduskills__title">
+                KEY SKILLS
             </div>
         </div>
     )
